67cabc3d68f1848083417187fda4a73d008853cd
János Székely <szekelyjani@gmail.com>
2026-06-29 10:25:57 +0200

    Initial Commit

---
+++ ProductStore/ProductStoreApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProductStoreApp.swift
+//  ProductStore
+//
+//  Created by Szekely Janos on 2026. 06. 29..
+//
+
+import SwiftUI
+
+@main
+struct ProductStoreApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
